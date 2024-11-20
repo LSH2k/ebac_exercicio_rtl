e10a6a309f27d6f049cede395707f9e26dc92647
Leandro Sousa <156814218+LSH2k@users.noreply.github.com>
2024-11-20 09:05:32 -0300

    Tests jest

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import PostComment from '.'
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+describe('Teste para o componente PostComments', () => {
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+
+  it('Deve inserir dois comentários', () => {
+    render(<PostComment />)
+
+    const textArea = screen.getByTestId('text-area-comment')
+    const button = screen.getByTestId('btn-comment')
+
+    fireEvent.change(textArea, { target: { value: 'Primeiro comentário' } })
+    fireEvent.click(button)
+
+    fireEvent.change(textArea, { target: { value: 'Segundo comentário' } })
+    fireEvent.click(button)
+
+    expect(screen.getByText('Primeiro comentário')).toBeInTheDocument()
+    expect(screen.getByText('Segundo comentário')).toBeInTheDocument()
+  })
+})
